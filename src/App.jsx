a9c097d8b39af5695f69b7a49a47ff8a9a39f9d2
Pratiti Paul <paulpratiti23@gmail.com>
2025-04-25 18:12:23 +0530

    starting again..delete all previous work

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,7 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
-import home from './components/home/home.jsx';
-import navbar from './components/navbar/navbar.jsx';
-import journal from './components/journal/journal.jsx';
-import selfcare from './components/selfcare/selfcare.jsx';
-import about from './components/about/about.jsx';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+// import { useState } from 'react'; 
+
+
 
 function App() {
   
