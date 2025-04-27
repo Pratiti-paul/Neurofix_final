c09239eb36598940db6372338c48b466e98b4238
Pratiti Paul <paulpratiti23@gmail.com>
2025-04-27 19:50:39 +0530

    moodcalendar started

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './home.css'; 
-import MoodCalendar from "../moodcalender/moodcalendar"; 
+import MoodCalendar from "../moodcalendar/moodcalendar"; 
 
 function Home() {
   return (
