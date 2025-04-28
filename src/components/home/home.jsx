b78045d20e43cd18c7cd03a3020f7917bab973c2
Pratiti Paul <paulpratiti23@gmail.com>
2025-04-28 09:59:48 +0530

    added an animation elemnet to the home page

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -1,8 +1,30 @@
 import React from 'react';
 import './home.css'; 
 import MoodCalendar from "../moodcalendar/moodcalendar"; 
+import FlowingMenu from '../animation/animation';
+
 
 function Home() {
+
+  const menuItems = [
+    {
+      link: "#",
+      text: "Mindfulness",
+      image: "https://images.unsplash.com/photo-1506744038136-46273834b3fb", // use any image
+    },
+    {
+      link: "#",
+      text: "Self Care",
+      image: "https://images.unsplash.com/photo-1523978591478-c753949ff840",
+    },
+    {
+      link: "#",
+      text: "Mood Journal",
+      image: "https://images.unsplash.com/photo-1518609878373-06d740f60d8b",
+    },
+  ];
+
+
   return (
     <div className="home-page">
       <div className="home-container">
@@ -11,6 +33,7 @@ function Home() {
           <p>Track your mental health and moods with us.</p>
         </div>
         <div className="calendar-section">
+          <FlowingMenu items={menuItems} />
           <MoodCalendar />
         </div>
       </div>
