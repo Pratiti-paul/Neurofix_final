bcb8cc0db8d60c18c85560deb558946d555d6e7c
Pratiti Paul <paulpratiti23@gmail.com>
2025-05-14 03:51:59 +0530

    starting with the about component

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -2,9 +2,20 @@ import { Link } from 'react-router-dom';
 
 function About() {
     return (
-      <div>
-        <h2>In Progress</h2>
+      <div className="card-container">
+      <div className="card">
+        <h2 className="card-title">Our Mission</h2>
+        <p className="card-text">
+        To support individuals on their mental wellness journey by providing a safe, calming digital space for daily reflection, emotional awareness, and self-care — empowering users to track their moods, journal their thoughts, and nurture personal growth with compassion and clarity.
+        </p>
       </div>
+      <div className="card">
+        <h2 className="card-title">Our Vision</h2>
+        <p className="card-text">
+        To create a world where mental health is prioritized and personalized — where every individual has the tools, insights, and support to understand themselves, manage their well-being, and thrive emotionally, every day.
+        </p>
+      </div>
+    </div>
     );
   }
   
